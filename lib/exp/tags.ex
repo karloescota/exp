bd53873aea312f49ec5079fd73635767d1ba7954
Karlo Escota <karlo.escota@gmail.com>
2021-06-07 18:03:06 +0800

    Add tags

---
+++ lib/exp/tags.ex
@@ -0,0 +1,32 @@
+defmodule Exp.Tags do
+  import Ecto.Query, warn: false
+  alias Exp.Repo
+
+  alias Exp.Tags.Tag
+
+  def list_tags do
+    Repo.all(Tag)
+  end
+
+  def get_tag!(id), do: Repo.get!(Tag, id)
+
+  def create_tag(attrs \\ %{}) do
+    %Tag{}
+    |> Tag.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def update_tag(%Tag{} = tag, attrs) do
+    tag
+    |> Tag.changeset(attrs)
+    |> Repo.update()
+  end
+
+  def delete_tag(%Tag{} = tag) do
+    Repo.delete(tag)
+  end
+
+  def change_tag(%Tag{} = tag, attrs \\ %{}) do
+    Tag.changeset(tag, attrs)
+  end
+end
