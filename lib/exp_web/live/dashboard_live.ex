0e7f732449594e14d8e41bee3354f4755e63daf2
Karlo Escota <karlo.escota@gmail.com>
2021-06-08 20:43:31 +0800

    Remove expenses data

--- lib/exp_web/live/dashboard_live.ex
+++ lib/exp_web/live/dashboard_live.ex
@@ -4,15 +4,11 @@ defmodule ExpWeb.DashboardLive do
   @impl true
   def mount(_params, %{"user_token" => _user_token} = session, socket) do
     socket = assign_defaults(socket, session)
-    current_user = socket.assigns.current_user
-
     date = Date.utc_today()
-    expenses = Exp.Expenses.list_expenses_for(current_user, date)
 
     {:ok,
      socket
      |> assign(year: date.year)
-     |> assign(month: date.month)
-     |> assign(expenses: expenses)}
+     |> assign(month: date.month)}
   end
 end
